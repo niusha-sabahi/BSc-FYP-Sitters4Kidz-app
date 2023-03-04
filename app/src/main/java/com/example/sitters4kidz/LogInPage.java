0a1dcab7fe8365017ca9025ba8dafdee79bc2473
Niush0110 <niushaesabahi@gmail.com>
2023-03-04 20:20:27 +0000

    v 1.1 - working on log in page input validation. NEXT: check if username and password entered have a match in db

--- app/src/main/java/com/example/sitters4kidz/LogInPage.java
+++ app/src/main/java/com/example/sitters4kidz/LogInPage.java
@@ -30,23 +30,47 @@ public class LogInPage extends AppCompatActivity {
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        //EditText username_inp;
-        //EditText password_inp;
+        EditText username_inp;
+        EditText password_inp;
 
         Button log_in_butt;
         //Button sig_nup_butt;
 
         //get inputs
-        //username_inp = (EditText) findViewById(R.id.username_inp);
-        //password_inp = (EditText) findViewById(R.id.password_inp);
+        username_inp = (EditText) findViewById(R.id.username_inp);
+        password_inp = (EditText) findViewById(R.id.password_inp);
 
         log_in_butt = (Button) findViewById(R.id.log_in_butt);
         log_in_butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                String username = username_inp.getText().toString();
+                String password = password_inp.getText().toString();
+
+                boolean user_found = false;
+
+
+                //move to home page when a log in match is found
+                Intent intent = new Intent(LogInPage.this,
+                        HomePage.class);
+                startActivity(intent);
+
+                if(user_found == false){
+                    showToast("the wrong password or username were entered. " +
+                            "please try again :).");
+                }
+
+
+
+
+
+
+
+
+
                 // Deletes a Document based on ID in a Collection
-                db.collection("users").document("gBJHaU8WBtQX8tYu8Ow6")
+                /*db.collection("cities").document("LA")
                         .delete()
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
@@ -59,8 +83,9 @@ public class LogInPage extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 Log.w("F", "Error deleting document", e);
                             }
-                        });
+                        });*/
 
+                /*
                 // creates and adds a new Document to a Collection with a specific ID
                 Map<String, Object> city = new HashMap<>();
                 city.put("name", "Los Angeles");
@@ -80,52 +105,16 @@ public class LogInPage extends AppCompatActivity {
                             public void onFailure(@NonNull Exception e) {
                                 Log.w("F", "Error writing document", e);
                             }
-                        });
-
-/*
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", "Ada");
-                user.put("last", "Lovelace");
-                user.put("born", 1815);
-
-                // Add a new document with a generated ID
-                db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("Success: ", "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("Failure: ", "Error adding document", e);
-                            }
                         });*/
-
-
-                /*Map<String, Object> users = new HashMap<>();
-                users.put("firstName", "Niusha");
-                users.put("lastName", "Sabahi");
-
-                db.collection("users").add(users);*/
-
-                /*firestore.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_LONG).show();
-                    }
-                });*/
 
             }
         });
 
     }
+
+    // A function for generating Toasts. To simplify code, and reduce repetition.
+    private void showToast(String text){
+        Toast.makeText(LogInPage.this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
